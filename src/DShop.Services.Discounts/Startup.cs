a6ecb9ba6823dbfc35cd8c2b93851b28703ab5a1
Mykola Pelekh <mpelekh@lohika.com>
2019-07-17 23:08:37 +0300

    Create Discount Domain and repository

--- src/DShop.Services.Discounts/Startup.cs
+++ src/DShop.Services.Discounts/Startup.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using DShop.Common.Dispatchers;
+using DShop.Common.Mongo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,24 +15,37 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using DShop.Common.Mvc;
+using DShop.Services.Discounts.Domain;
 
 namespace DShop.Services.Discounts
 {
     public class Startup
     {
         public IConfiguration Configuration { get; }
+        public IContainer Container { get; }
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddCustomMvc();
+            services.AddInitializers(typeof(IMongoDbInitializer));
+            
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
+                .AsImplementedInterfaces();
+            builder.Populate(services);
+            builder.AddDispatchers();
+            builder.AddMongo();
+            builder.AddMongoRepository<Discount>("Discounts");
+            
+            return new AutofacServiceProvider(Container);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment() || env.EnvironmentName == "local")
             {
@@ -35,6 +53,8 @@ namespace DShop.Services.Discounts
             }
 
             app.UseMvc();
+
+            applicationLifetime.ApplicationStopped.Register(() => Container.Dispose());
         }
     }
 }
